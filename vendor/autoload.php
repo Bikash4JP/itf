e203b150877fbccbbdbe05dd30d87a5db2bd1061
bikash4jp <bikash4jp@gmail.com>
2025-07-07 09:52:13 +0900

    update

--- vendor/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Manual autoload for PhpSpreadsheet and Dompdf
-require_once __DIR__ . '/phpoffice/phpspreadsheet/autoload.php';
-require_once __DIR__ . '/dompdf/dompdf/autoload.inc.php';
-?>
